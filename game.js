2ac8fee825415c6e09cffec28275323c74e1464d
Arcan3s <155115078+Arcan3s@users.noreply.github.com>
2024-04-01 18:44:21 -0400

    Add win and lose condition for playGame()

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
     const randomChoice = Math.floor(Math.random() * choice.length);
-    console.log(choice[randomChoice]);
     return choice[randomChoice]; // randomChoice is index number in choice array from 0 to 2
 }
 
@@ -20,15 +19,18 @@ function playRound(playerSelection, computerSelection) {
         "Win";
     }
 }
-
+// Make it so player has to re-enter if not a valid option
 function playGame() {
     let score = 0;
+    let round = 1;
     for (let i = 0; i < 5; i++) {
+        console.log(`Round: ${round}`);
+        round += 1;
         let computerSelection = getComputerChoice();
         let playerSelection = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
-        console.log(playerSelection);
+        console.log(`You: ${playerSelection}`);
+        console.log(`Computer: ${computerSelection}`);
         let result = playRound(playerSelection, computerSelection);
-        console.log(result);
         if (result === "Win") {
             score += 1;
             console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
@@ -37,10 +39,10 @@ function playGame() {
         } else {
             console.log(`It's a Tie! ${playerSelection} can't beat itself...`)
         }
-        console.log(score);
-
+        console.log(`Score: ${score}`);
     }
+    return score >= 3 ? "WINNER!" :
+    "LOSER!";
 }
 
-playGame();
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playGame());
