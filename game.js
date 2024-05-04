5d309a81af60e62114e8093a828f1866cd1b70cf
Arcan3s <155115078+Arcan3s@users.noreply.github.com>
2024-05-04 12:56:50 -0400

    Add computer score tracker, edit win condition

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ function getComputerChoice() {
     return choice[randomChoice]; // randomChoice is index number in choice array from 0 to 2
 }
 
+function getHumanChoice() {
+    const humanChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
+    return humanChoice;
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         return computerSelection === playerSelection ? "Tie" :
@@ -21,28 +26,39 @@ function playRound(playerSelection, computerSelection) {
 }
 // Make it so player has to re-enter if not a valid option
 function playGame() {
-    let score = 0;
+    let playerScore = 0;
+    let computerScore = 0;
     let round = 1;
     for (let i = 0; i < 5; i++) {
         console.log(`Round: ${round}`);
         round += 1;
         let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
+        let playerSelection = getHumanChoice();
         console.log(`You: ${playerSelection}`);
         console.log(`Computer: ${computerSelection}`);
         let result = playRound(playerSelection, computerSelection);
         if (result === "Win") {
-            score += 1;
+            playerScore += 1;
             console.log(`You Win! ${playerSelection} beats ${computerSelection}.`)
         } else if (result === "Lose") {
+            computerScore += 1;
             console.log(`You Lose! ${computerSelection} beats your ${playerSelection}.`)
         } else {
             console.log(`It's a Tie! ${playerSelection} can't beat itself...`)
         }
-        console.log(`Score: ${score}`);
+        console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
     }
-    return score >= 3 ? "WINNER!" :
-    "LOSER!";
+    // return playerScore > computerScore ? "WINNER!" :
+    // "LOSER!";
+    return playerScore > computerScore ? "WINNER!" : playerScore === computerScore ? "TIE GAME!" : "LOSER!";
 }
 
 console.log(playGame());
+
+// function convertInput() {
+// 	LET playerSelection = prompt().toLowerCase()
+// 	IF playerSelection !== "rock" || "paper" || "scissors"
+// 		playerSelection
+// 	ELSE
+// 		RETURN playerSelection
+// }
